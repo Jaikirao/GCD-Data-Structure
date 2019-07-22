7eb39ff5b481e33d228a8db45e1dccb8f32b21b9
Jaikirao <48488555+Jaikirao@users.noreply.github.com>
2019-07-22 23:47:46 +0530

    Create GCD_2.py
    it is a another way to find out the gcd no.

---
+++ GCD_2.py
@@ -0,0 +1,12 @@
+#this is the secound way to find out the GCD no. 
+def gcd(m,n):
+  #let suppose m>=n
+  if m<n:
+    #we just simply swap the no. if it is wrong.
+    (m,n)=(n,m)
+    
+  if (m%n)==0:
+    return (n)
+  else: 
+    diff =m-n
+    return (gcd(max(m-n),min(m-n)))
